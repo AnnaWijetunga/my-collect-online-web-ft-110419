f35e898aab3a4215ccc4c3eed77967d5ccbfd777
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 02:12:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,18 @@
-def my_collect(empty_array)
-  i = 0 
-  new_collection = []
-  while i < collection.length 
-    new_collection << yield(collection[i])
-    i += 1 
-  end
-end
+# def my_collect(empty_array)
+#   i = 0 
+#   new_collection = []
+#   while i < collection.length 
+#     new_collection << yield(collection[i])
+#     i += 1 
+#   end
+# end
 
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
